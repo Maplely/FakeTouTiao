51b7aede36da824e11781be8798ca133325c07ce
HighTop <986086927@qq.com>
2018-07-09 14:28:36 +0800

    add

--- app/src/main/java/toutiao/fake/com/faketoutiao/ui/activity/MainActivity.java
+++ app/src/main/java/toutiao/fake/com/faketoutiao/ui/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package toutiao.fake.com.faketoutiao.ui.activity;
 
 import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
